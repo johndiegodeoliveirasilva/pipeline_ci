3ce466652b39a99ec1892c33ccfba1e95c30421b
John Diego de Oliveira Silva <johnoliveirasilva9@gmail.com>
2025-02-20 17:00:09 -0300

    sobelow confs

--- mix.exs
+++ mix.exs
@@ -67,7 +67,8 @@ defmodule PipelineCi.MixProject do
       {:dns_cluster, "~> 0.1.1"},
       {:bandit, "~> 1.5"},
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
-      {:excoveralls, "~> 0.18", only: :test}
+      {:excoveralls, "~> 0.18", only: :test},
+      {:sobelow, "~> 0.13", only: [:dev, :test]}
     ]
   end
 
